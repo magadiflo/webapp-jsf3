72b33337f8d74f47466e5c0683556f967b324955
Martín <magadiflo@gmail.com>
2022-01-21 19:43:29 -0500

    Configurando archivos de idiomas properties (Resource Bundle)

--- src/main/java/org/magadiflo/webapp/jsf3/ProducerResources.java
+++ src/main/java/org/magadiflo/webapp/jsf3/ProducerResources.java
@@ -4,6 +4,10 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @ApplicationScoped
 public class ProducerResources {
@@ -14,6 +18,13 @@ public class ProducerResources {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         facesContext.getExternalContext().getFlash().setKeepMessages(true);
         return facesContext;
+    }
+
+    @Produces
+    @Named("msg")
+    public ResourceBundle beanBundle() {
+        Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+        return ResourceBundle.getBundle("messages", locale);
     }
 
 }
